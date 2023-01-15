7517fdb5066a90f91cc4c9ef1efe454c67d63864
Sam <samcope24@gmail.com>
2023-01-15 18:12:17 +0000

    add web server + security group to EC2 instance

--- main.tf
+++ main.tf
@@ -3,10 +3,34 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id] // links the EC2 instance to the security group
+
+  // script that runs on startup and launches a websever
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello World!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+// aws securtiy group to allow incoming requests on port 8080 from any IP
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    description      = "Example Security Group"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
