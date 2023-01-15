69d3452b8a096c34ef0e7b1a1863cbe25f113059
Sam <samcope24@gmail.com>
2023-01-15 21:22:22 +0000

    add EC2 instances to an auto-scaling group

--- main.tf
+++ main.tf
@@ -2,10 +2,23 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0fb653ca2d3203ac1"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id] // links the EC2 instance to the security group
+// acts as a datasource for the default aws vpc
+data "aws_vpc" "default" {
+  default = true
+}
+
+// used to pull subnet data from the default vpc
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id] // links the EC2 instance to the security group
 
   // script that runs on startup and launches a websever
   user_data = <<-EOF
@@ -14,10 +27,24 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-  user_data_replace_on_change = true
+  // Required when using a launch configuration with an auto scaling group 
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  tags = {
-    Name = "terraform-example"
+// creation of an auto scaling group that runs between 2 and 10 EC2 instances
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -38,12 +65,6 @@ resource "aws_security_group" "instance" {
 // variable for sotring the port number in a single place
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  type = number
-  default = 8080
-}
-
-// output varaibles can be used to access values once resources have been created
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
+  type        = number
+  default     = 8080
 }
