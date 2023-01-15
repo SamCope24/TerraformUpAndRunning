d0fb458e87eed39b359c81882cb767c2445171fe
Sam <samcope24@gmail.com>
2023-01-15 22:04:39 +0000

    add an application load balancer

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = "us-east-2"
 }
 
-// acts as a datasource for the default aws vpc
+# acts as a datasource for the default aws vpc
 data "aws_vpc" "default" {
   default = true
 }
 
-// used to pull subnet data from the default vpc
+# used to pull subnet data from the default vpc
 data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
@@ -18,27 +18,30 @@ data "aws_subnets" "default" {
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0fb653ca2d3203ac1"
   instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id] // links the EC2 instance to the security group
+  security_groups = [aws_security_group.instance.id] # links the EC2 instance to the security group
 
-  // script that runs on startup and launches a websever
+  # script that runs on startup and launches a websever
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello World!" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-  // Required when using a launch configuration with an auto scaling group 
+  # Required when using a launch configuration with an auto scaling group 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-// creation of an auto scaling group that runs between 2 and 10 EC2 instances
+# creation of an auto scaling group that runs between 2 and 10 EC2 instances
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnets.default.ids
 
-  min_size = 2
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 3
   max_size = 10
 
   tag {
@@ -48,7 +51,7 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
-// aws securtiy group to allow incoming requests on port 8080 from any IP
+# aws securtiy group to allow incoming requests on port 8080 from any IP
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
@@ -62,9 +65,103 @@ resource "aws_security_group" "instance" {
   }
 }
 
-// variable for sotring the port number in a single place
+# variable for sotring the port number in a single place
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
   default     = 8080
+}
+
+# creates the application load balancer
+resource "aws_alb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+# creates the ALB listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_alb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # by default return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+# by default ALB does not allow any incoming/outgoing traffic so need a SG
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
+  ingress {
+    description      = "ALB Security Group"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    description      = "ALB Security Group"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+# create a target group for our ASG
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  # health checks our EC2 instances periodically and only considers an instance
+  # as healthy if we get a 200 OK response as defined by our matcher
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# creates the ALB listener rules which send requests that match any path
+# to the target group that contains our ASG
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+output "alb_dns_name" {
+  value       = aws_alb.example.dns_name
+  description = "The domain name of the load balancer"
 }
