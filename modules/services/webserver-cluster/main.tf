16ec900b6e2484edc9cf2f3183a5c2d7e2f4c6f2
Sam <samcope24@gmail.com>
2023-01-22 17:34:35 +0000

    introduce the webserver cluster as a reusable module

---
+++ modules/services/webserver-cluster/main.tf
@@ -0,0 +1,175 @@
+# acts as a datasource for the default aws vpc
+data "aws_vpc" "default" {
+  default = true
+}
+
+# used to pull subnet data from the default vpc
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id] # links the EC2 instance to the security group
+
+  # script that runs on startup and launches a websever, rendered via a template 
+  user_data = templatefile("user-data.sh", {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  })
+
+  # Required when using a launch configuration with an auto scaling group 
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# creation of an auto scaling group that runs between 2 and 10 EC2 instances
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 3
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+# aws securtiy group to allow incoming requests on port 8080 from any IP
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    description      = "Example Security Group"
+    from_port        = var.server_port
+    to_port          = var.server_port
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+# creates the application load balancer
+resource "aws_alb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+# creates the ALB listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_alb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # by default return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+# by default ALB does not allow any incoming/outgoing traffic so need a SG
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
+  ingress {
+    description      = "ALB Security Group"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    description      = "ALB Security Group"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+# create a target group for our ASG
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  # health checks our EC2 instances periodically and only considers an instance
+  # as healthy if we get a 200 OK response as defined by our matcher
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# creates the ALB listener rules which send requests that match any path
+# to the target group that contains our ASG
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+# configure terraform to store state in an S3 bucket
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-5829cbe9"
+    key    = "stage/services/webserver-cluster/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+# read outputs from the database's state file 
+# all od the database's output variables are stored in the state file
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+  config = {
+    bucket = "terraform-up-and-running-state-5829cbe9"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
