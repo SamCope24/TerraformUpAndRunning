db7502f9ae7cbee57c5f98f286d7d3851d512633
Sam <samcope24@gmail.com>
2023-01-22 14:03:06 +0000

    set up an RDS instance

---
+++ stage/data-stores/mysql/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  skip_final_snapshot = true
+  db_name             = "example_database"
+
+  username = var.db_username
+  password = var.db_password
+}
+
+# configure terraform to store state in an S3 bucket
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-5829cbe9"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
