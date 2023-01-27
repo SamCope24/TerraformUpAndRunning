23ff155d9b58587a31eef7be155554f6ea8815b8
Sam <samcope24@gmail.com>
2023-01-27 09:34:27 +0000

    use git tag to pull in terraform module rather than local reference

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "git@github.com:SamCope24/TerraformUpAndRunningModules.git//services/webserver-cluster?ref=v0.0.1"
 
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-up-and-running-state-5829cbe9"
