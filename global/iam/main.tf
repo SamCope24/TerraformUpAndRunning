d5cdbae0005316fb2bb0a733af98185c966969a3
Sam <samcope24@gmail.com>
2023-01-30 08:28:37 +0000

    use iam module to create multiple users

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -2,23 +2,20 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_iam_user" "example" {
-    count = length(var.user_names)
-    name = var.user_names[count.index]
+module "users" {
+  source = "../../../modules/landing-zone/iam-user"
+
+  count     = length(var.user_names)
+  user_name = var.user_names[count.index]
 }
 
 variable "user_names" {
   description = "Create IAM users with these names"
-  type = list(string)
-  default = [ "neo", "trinity", "morpheus" ]
-}
-
-output "first_arn" {
-  value = aws_iam_user.example[0].arn
-  description = "The ARN for the first user"
+  type        = list(string)
+  default     = ["neo", "trinity", "morpheus"]
 }
 
 output "all_arns" {
-  value = aws_iam_user.example[*].arn
+  value       = module.users[*].user_arn
   description = "The ARNs for all users"
 }
