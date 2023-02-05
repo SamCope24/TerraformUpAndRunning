07db532cf238545f041d292d95bd44d8096281fe
Sam <samcope24@gmail.com>
2023-02-05 21:43:29 +0000

    start using for loops

--- global/iam/outputs.tf
+++ global/iam/outputs.tf
@@ -7,3 +7,15 @@ output "all_arns" {
   value       = module.users.all_arns
   description = "The ARNs for all users"
 }
+
+output "upper_names" {
+  value = [for name in var.user_names : upper(name) if length(name) < 5]
+}
+
+output "bios" {
+  value = [for name, role in var.hero_thousand_faces : "${name} is the ${role}"]
+}
+
+output "upper_roles" {
+  value = {for name, role in var.hero_thousand_faces : upper(name) => upper(role)}
+}
